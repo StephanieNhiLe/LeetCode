186a2f447c612a2e8475947012c79864e142f938
Stephanie Nhi Le <87336144+StephanieNhiLe@users.noreply.github.com>
2024-06-13 17:44:48 -0500

    :ballot_box_with_check: shifting-letters [Runtime: 5 ms; Memory: 55.3 MB]

--- prefix-sum/shifting-letters.java
+++ prefix-sum/shifting-letters.java
@@ -1,12 +1,10 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
         char[] chars = s.toCharArray();
-        long totalShifts = 0; 
+        long sumShift = 0;
         for (int i = shifts.length - 1; i >= 0; i--) {
-            totalShifts += shifts[i];
-            totalShifts %= 26; 
-            int index = chars[i] - 'a';
-            chars[i] = (char) ((index + totalShifts) % 26 + 'a');
+            sumShift += shifts[i];
+            chars[i] = (char) ((chars[i] - 'a' + sumShift) % 26 + 'a');
         }
         return new String(chars);
     }
