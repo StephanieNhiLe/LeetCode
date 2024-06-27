b44339becfd27e073ef295e2eea9493110e03a5a
Stephanie Nhi Le <87336144+StephanieNhiLe@users.noreply.github.com>
2024-06-27 16:21:41 -0500

    :ballot_box_with_check: longest-substring-without-repeating-characters [Runtime: 5 ms; Memory: 44.3 MB]

--- hash-table/longest-substring-without-repeating-characters.java
+++ hash-table/longest-substring-without-repeating-characters.java
@@ -1,14 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) return 0; 
+        if (s.length() == 0) return 0;  
         HashMap<Character, Integer> map = new HashMap<>();
         int max = 0;
-        for (int i = 0, j= 0; j < s.length(); j++){
-            if (map.containsKey(s.charAt(j))){
-                i = Math.max(map.get(s.charAt(j)) + 1, i);
+        int start = 0;
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (map.containsKey(c) && map.get(c) >= start){
+                start = map.get(c) + 1;
             }
-            map.put(s.charAt(j), j);
-            max = Math.max(max, j - i + 1);
+            map.put(c, i);
+            max = Math.max(max, i - start + 1);
         }
         return max;
     }
